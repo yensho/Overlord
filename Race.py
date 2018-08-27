e3e9bfe6a5be0ef6c4a2bca51d51f0500312ad1b
yensho <yensho@users.noreply.github.com>
2018-08-26 22:58:44 -0500

    Start of Project

---
+++ Race.py
@@ -0,0 +1,11 @@
+class Race:
+
+    def __init__(self):
+        self.BaseStr = 0
+        self.BaseDex = 0
+        self.BaseVit = 0
+        self.BaseInt = 0
+        self.BaseMnd = 0
+        self.BaseChr = 0
+        self.Weaknesses = []
+        self.Resistances = []
